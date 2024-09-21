75af2eaa0204ece2e20bcb2bfe1492d1ed2018bf
Facundo Lobo <facundolobo2@gmail.com>
2024-09-21 03:16:30 -0300

    back basico listo

---
+++ instrucciones creacion backend.sql
@@ -0,0 +1,17 @@
+--Primeros pasos 
+1 - Creacion de una carpeta del proyecto 
+abrir cmd en la direcccion de la carpeta 
+ejecutar : 
+> npm init -y
+creara el package.json
+
+2 - instalacion de nodemon para ejecutar app 
+> npm install -g nodemon
+Es una instlacion global. 
+
+3 - instalacion de express dotenv 
+> npm i express dotenv 
+
+4 -instalacion de cors
+
+
