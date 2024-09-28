028f2508c7bbb2c81f6afa9e30637c1cb74ea313
Facundo Lobo <facundolobo2@gmail.com>
2024-09-28 02:36:24 -0300

    act

--- herramientas/Instrucciones.sql
+++
@@ -1,4 +0,0 @@
-1 - Crear la base de datos, con todos sus registros, ejecutando 'bdclientes.sql'
-2 - Importar la coleccion en postman para el llamado de Apis desde 'RestServer-Mysql-Login.postman_collection' y despues las variables de entorno 'Rest-server.postman_environment.json'
-3 - ejecutar npm install en el proyecto rest-server
-4 - Revisar las credenciales de base de datos en el archivo .env
